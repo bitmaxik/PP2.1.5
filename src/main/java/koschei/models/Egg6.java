bf16fcc293754f7ec81506f8a96de9926b2275fa
bitmaxik <bitmaxik@gmail.com>
2023-08-16 23:39:39 +0300

    add PP2.1.5

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -1,9 +1,16 @@
 package koschei.models;
 
+
 public class Egg6 {
+
+    private Deth8 deth;
+
+    public Egg6(Deth8 deth) {
+        this.deth = deth;
+    }
 
     @Override
     public String toString() {
-        return ", в яйце иголка " + "";
+        return ", в яйце иголка " + deth.toString();
     }
 }
